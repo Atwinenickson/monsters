46af397c9ece875bad3bcacb3b2b1e3252eef4a4
Atwinenickson <atwiinenicxon@gmail.com>
2019-12-15 23:08:08 +0300

    Updates

---
+++ precache-manifest.1b02522cead8786cd4154b609f40304e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d622fc0caa7b529c845a4de49c7e071",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "62999d22f9b08c23bab3",
+    "url": "/monsters/static/css/main.053e3b50.chunk.css"
+  },
+  {
+    "revision": "5ffb6fab3c6fb8afc674",
+    "url": "/monsters/static/js/2.f6373902.chunk.js"
+  },
+  {
+    "revision": "62999d22f9b08c23bab3",
+    "url": "/monsters/static/js/main.57b8677a.chunk.js"
+  },
+  {
+    "revision": "b29c299a2556335a6eb0",
+    "url": "/monsters/static/js/runtime-main.02df2f2e.js"
+  }
+]);
